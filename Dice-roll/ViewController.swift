87f5db89272d3b8e1e78faed6fc8d868261863d6
Subash Pradahan <suprowilde5@gmail.com>
2020-01-03 12:13:04 +0100

    Initial Commit

---
+++ Dice-roll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice-roll
+//
+//  Created by Subash Pradhan on 03/01/2020.
+//  Copyright © 2020 Subash Pradhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
