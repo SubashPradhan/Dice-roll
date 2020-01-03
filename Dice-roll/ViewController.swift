364e5ddff818cca6cc93dc638763040bb889e523
Subash Pradahan <suprowilde5@gmail.com>
2020-01-03 12:41:21 +0100

    Adding function to the button

--- Dice-roll/ViewController.swift
+++ Dice-roll/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageLeft: UIImageView!
+    @IBOutlet weak var diceImageRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        let diceImages = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageLeft.image = diceImages.randomElement()
+        diceImageRight.image = diceImages.randomElement()
+    }
+    
 }
 
